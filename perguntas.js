e572939b22f398d68c77e504ae526f5460ad6c5c
anonimo504a <136383187+anonimo504a@users.noreply.github.com>
2024-09-16 17:01:19 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'biotecnologia',
-    'o que é a biotecnologia verde?',
+    'Biotecnologia',
+    'O que é a biotecnologia verde?',
     'É a biotecnologia da agricultura'
 )
 
 criaCartao(
-    'biologia',
-    'quais plantas são gimnospermas?',
-    'araucárias, cedros, ciprestes, pinheiros e sequóias'
+    'Biologia',
+    'Quais plantas são gimnospermas?',
+    'Araucárias, cedros, ciprestes, pinheiros e sequóias'
 )
 
 criaCartao(
-    'quimica',
-    'qual elemento tem como simbolo "P"?',
-    'fósforo'
+    'Quimica',
+    'Qual elemento tem como simbolo "P"?',
+    'Fósforo'
 )
 
 criaCartao(
-    'quimica',
-    'o que é H2O?',
-    'água a sigla significa que ela possui duas moléculas de hidrogênio (H) e uma de oxigênio (O)'
+    'Quimica',
+    'O que é H2O?',
+    'Água a sigla significa que ela possui duas moléculas de hidrogênio (H) e uma de oxigênio (O)'
 )
